903853330636bd2f444bf07f97ec02234fd17425
Aldana-dev <aldana.devmail@gmail.com>
2025-06-12 20:20:49 -0300

    Configuracion del proyecto y página principal

---
+++ app/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/nosotras')
+def nosotras():
+    return render_template('nosotras.html')
+
+@app.route('/tienda')
+def tienda():
+    return render_template('tienda.html')
+
+@app.route('/marcos')
+def marcos():
+    return render_template('marcos.html')
+
+@app.route('/galeria')
+def galeria():
+    return render_template('galeria.html')
+
+@app.route('/carrito')
+def carrito():
+    return render_template('carrito.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
